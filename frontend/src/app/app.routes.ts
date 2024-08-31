f5699b66ebec7579771f6802aa7567c5d8794e63
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-08-31 10:15:12 -0300

    feat: Add profile form component and update dashboard profile component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { RegisterPageComponent } from './components/register_page/register-page.component';
 import { LoginPageComponent } from './components/login_page/login-page.component';
 import { ForgotPasswordPageComponent } from './components/forgot_password_page/forgot-password-page.component';
-import { DashboardQuotationComponent } from './components/dashboard-quotation/dashboard-quotation.component';
-import { DashboardNotificationsComponent } from './components/dashboard-notifications/dashboard-notifications.component';
-import { DashboardProfileComponent } from './components/dashboard-profile/dashboard-profile.component';
+import { DashboardQuotationComponent } from './components/dashboard_quotation/dashboard-quotation.component';
+import { DashboardNotificationsComponent } from './components/dashboard_notifications/dashboard-notifications.component';
+import { DashboardProfileComponent } from './components/dashboard_profile/dashboard-profile.component';
 
 export const routes: Routes = [
   { path: '', component: LoginPageComponent }, // Home

47f1239bd7928e9ed15d9079b32c79e9e5212c21
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-01 10:27:19 -0300

    chore: Update ngx-mask dependency to version 18.0.0

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
+    // Fornece o serviço para configuração do ngx-mask
+    provideEnvironmentNgxMask(),
   ],
 };
--- frontend/src/app/components/quotation_form/form-dashboard.component.ts
+++ frontend/src/app/components/quotation_form/form-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @Component({
   selector: 'app-form-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgxMaskDirective, NgxMaskPipe],
   templateUrl: './form-dashboard.component.html',
 })
 export class FormDashboardComponent {}

48bce240eb2b31e19c8e4e5cba93d1af19b433e5
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-03 09:08:33 -0300

    feat: Update sidebar component to include active state for links

--- frontend/src/app/components/sidebar_dashboard/sidebar-dashboard.component.ts
+++ frontend/src/app/components/sidebar_dashboard/sidebar-dashboard.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLinkActive, RouterModule } from '@angular/router';
 
+// Decorador para definir as propriedades do componente
 @Component({
-  selector: 'app-sidebar-dashboard',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './sidebar-dashboard.component.html',
-  styleUrls: ['./sidebar-dashboard.component.css'],
+  selector: 'app-sidebar-dashboard', // Nome do seletor usado para o componente
+  standalone: true, // Define que o componente é independente, não precisa de um módulo Angular específico
+  imports: [RouterModule, RouterLinkActive], // Importa módulos necessários para roteamento e ativação de links
+  templateUrl: './sidebar-dashboard.component.html', // Caminho para o template HTML do componente
+  styleUrls: ['./sidebar-dashboard.component.css'], // Caminho para o arquivo de estilos CSS do componente
 })
 export class SidebarDashboardComponent {
+  // Propriedade para controlar o estado de expansão do sidebar
   isExpanded = false;
 
+  // Método para alternar o estado de expansão do sidebar
   toggleSidebar() {
+    // Inverte o valor da propriedade isExpanded
     this.isExpanded = !this.isExpanded;
   }
 }

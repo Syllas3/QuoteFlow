e0c54e9fa67e8b42aa527eb5e10cfc68f119913a
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-03 11:40:00 -0300

    refactor: Update component names and imports for quotation form dashboard

--- frontend/src/app/components/dashboard_quotation/dashboard-quotation.component.ts
+++ frontend/src/app/components/dashboard_quotation/dashboard-quotation.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { SidebarDashboardComponent } from "../sidebar_dashboard/sidebar-dashboard.component";
-import { FormDashboardComponent } from "../quotation_form/form-dashboard.component";
+import { SidebarDashboardComponent } from '../sidebar_dashboard/sidebar-dashboard.component';
+import { FormDashboardComponent } from '../quotation_form/quotation-form-dashboard.component';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-dashboard-quotation',
   standalone: true,
   imports: [SidebarDashboardComponent, FormDashboardComponent],
   templateUrl: './dashboard-quotation.component.html',
-  styleUrl: './dashboard-quotation.component.css'
+  styleUrl: './dashboard-quotation.component.css',
 })
-export class DashboardQuotationComponent {
-
-}
+export class DashboardQuotationComponent {}
--- frontend/src/app/components/quotation_form/quotation-form-dashboard.component.ts
+++ frontend/src/app/components/quotation_form/quotation-form-dashboard.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @Component({
-  selector: 'app-form-dashboard',
+  selector: 'app-quotation-form-dashboard',
   standalone: true,
   imports: [NgxMaskDirective, NgxMaskPipe],
-  templateUrl: './form-dashboard.component.html',
+  templateUrl: './quotation-form-dashboard.component.html',
 })
 export class FormDashboardComponent {}

29ce373bb4d044ac3fff1aab8eae2695ae48c287
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-03 13:32:39 -0300

    refactor: Update styles and dependencies for notifications form and index.html

--- frontend/src/app/components/notifications_form/notifications-form.component.ts
+++ frontend/src/app/components/notifications_form/notifications-form.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './notifications-form.component.html',
-  styleUrl: './notifications-form.component.css'
 })
 export class NotificationsFormComponent {
 

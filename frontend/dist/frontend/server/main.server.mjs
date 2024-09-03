47879f6885fa48de7912b5bcb76149f8df9920e8
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-03 11:10:51 -0300

    chore: Update .gitignore and form-dashboard component

---
+++ frontend/dist/frontend/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-LQZDXRK5.mjs";import"./chunk-PAARWYJ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- frontend/src/app/components/quotation_form/form-dashboard.component.ts
+++ frontend/src/app/components/quotation_form/form-dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
-
 @Component({
   selector: 'app-form-dashboard',
   standalone: true,

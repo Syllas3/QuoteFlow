b20f6ca97232355b3af46af1839ffc88a74dd935
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-04 01:21:27 -0300

    refactor: Update styles and dependencies for notifications form and index.html

--- frontend/dist/frontend/server/main.server.mjs
+++ frontend/dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JJ6GLKXU.mjs";import"./chunk-PAARWYJ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-LG7IFTS3.mjs";import"./chunk-PAARWYJ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};

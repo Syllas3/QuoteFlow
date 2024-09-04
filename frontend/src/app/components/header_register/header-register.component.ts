e68e9a4efa02f6a44e1bb2bf2b4bf1317797adb9
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-04 16:42:49 -0300

    refactor: Update header component toggler classes for improved styling

--- frontend/src/app/components/header_register/header-register.component.ts
+++ frontend/src/app/components/header_register/header-register.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header-register.component.html',
   styleUrl: './header-register.component.css'
 })
-export class HeaderRegisterComponent {}
+export class HeaderRegisterComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}

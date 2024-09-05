0381e1d22e976ee49377ec2baaaeb65afab67200
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-05 10:58:51 -0300

    Update notifications form component and template

--- frontend/src/app/components/notifications_form/notifications-form.component.ts
+++ frontend/src/app/components/notifications_form/notifications-form.component.ts
@@ -1,17 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-notifications-form',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './notifications-form.component.html',
+  selector: 'app-notifications-form', // Define o seletor do componente
+  standalone: true, // Define que este é um componente autônomo (standalone)
+  imports: [CommonModule, RouterModule], // Importa os módulos necessários (CommonModule e RouterModule)
+  templateUrl: './notifications-form.component.html', // Caminho do arquivo de template HTML
 })
 export class NotificationsFormComponent {
-  User: string = "Lucas";
+  showNotificationAlert: boolean = true; // Controla a exibição do alerta de notificações
 
+  // Função chamada quando o botão de fechar alerta é clicado
+  closeButton() {
+    this.showNotificationAlert = false; // Esconde o alerta ao definir a variável como falsa
+  }
+
+  User: string = 'Lucas'; // Armazena o nome do usuário para exibição personalizada
+
+  // Array de notificações que são exibidas na interface
   Notifications: Array<string> = [
-    "Para realizar cotações em nosso sistema, primeiro é necessário cadastrar o seu CNPJ no seu perfil.",
-    "Para2 realizar2 cotações2 em2 nosso2 sistema2, primeiro2 é2 necessário2 cadastrar2 o2 seu2 CNPJ2 no2 seu2 perfil2."
+    'Para realizar cotações em nosso sistema, primeiro é necessário cadastrar o seu CNPJ no seu perfil.',
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est modi inventore libero nisi sunt rem nemo quaerat velit. Expedita, ab impedit dolorum rem a adipisci magni minus mollitia nam maxime!',
   ];
 }

d5b1386a8368f3ddf543400d63da8ce44b9fdba7
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-04 21:45:34 -0300

    Update notifications-form.component.ts

--- frontend/src/app/components/notifications_form/notifications-form.component.ts
+++ frontend/src/app/components/notifications_form/notifications-form.component.ts
@@ -9,4 +9,8 @@ import { RouterModule } from '@angular/router';
 })
 export class NotificationsFormComponent {
   User: String = "Lucas";
+
+  Notifications1: Array[] = "Estamos felizes em tê-lo conosco! Se você está lendo esta mensagem,
+          desejamos que aproveite ao máximo nosso sistema de cotação."
+  Notifications2: Array[]: 
 }

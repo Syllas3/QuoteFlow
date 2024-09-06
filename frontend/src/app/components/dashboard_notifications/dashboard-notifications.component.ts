5f92759840412c8f51edc86399e547724d523ee7
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-06 16:04:48 -0300

    Refactor dashboard notifications component: Update modal functionality and add localStorage check

--- frontend/src/app/components/dashboard_notifications/dashboard-notifications.component.ts
+++ frontend/src/app/components/dashboard_notifications/dashboard-notifications.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-import { SidebarDashboardComponent } from "../sidebar_dashboard/sidebar-dashboard.component";
-import { NotificationsFormComponent } from "../notifications_form/notifications-form.component";
+import { SidebarDashboardComponent } from '../sidebar_dashboard/sidebar-dashboard.component';
+import { NotificationsFormComponent } from '../notifications_form/notifications-form.component';
 
 @Component({
   selector: 'app-dashboard-notifications',
   standalone: true,
   imports: [SidebarDashboardComponent, NotificationsFormComponent],
   templateUrl: './dashboard-notifications.component.html',
-  styleUrls: ['./dashboard-notifications.component.css']
+  styleUrls: ['./dashboard-notifications.component.css'],
 })
 export class DashboardNotificationsComponent implements OnInit {
-
   constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
-    const modalElement = this.el.nativeElement.querySelector('#modalNotifications');
-    const modal = (window as any).bootstrap.Modal.getOrCreateInstance(modalElement);
-    modal.show();
+    // Verifica se o modal já foi exibido
+    const hasModalShown = localStorage.getItem('modalShown');
+
+    if (!hasModalShown) {
+      // Se o modal ainda não foi exibido, obtém o elemento modal
+      const modalElement = this.el.nativeElement.querySelector(
+        '#modalNotifications'
+      );
+
+      // Inicializa o modal do Bootstrap
+      const modal = (window as any).bootstrap.Modal.getOrCreateInstance(
+        modalElement
+      );
+
+      // Exibe o modal
+      modal.show();
+
+      // Salva no localStorage que o modal foi exibido
+      localStorage.setItem('modalShown', 'true');
+    }
   }
 }

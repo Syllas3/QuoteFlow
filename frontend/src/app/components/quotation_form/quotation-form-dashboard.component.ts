e57efaceabe1dbe619982320fd2d0415ea9c58d7
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-06 16:45:38 -0300

    Refactor quotation form component: Add form validation and submission functionality

--- frontend/src/app/components/quotation_form/quotation-form-dashboard.component.ts
+++ frontend/src/app/components/quotation_form/quotation-form-dashboard.component.ts
@@ -1,10 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @Component({
   selector: 'app-quotation-form-dashboard',
   standalone: true,
-  imports: [NgxMaskDirective, NgxMaskPipe],
+  imports: [NgxMaskDirective, NgxMaskPipe, ReactiveFormsModule, CommonModule],
   templateUrl: './quotation-form-dashboard.component.html',
 })
-export class FormDashboardComponent {}
+export class FormDashboardComponent {
+  quotationForm: FormGroup; // Formulário gerenciado pelo FormGroup
+
+  constructor(private fb: FormBuilder) {
+    // Inicialização do formulário com campos e validações
+    this.quotationForm = this.fb.group({
+      categoria: ['', Validators.required], // Campo obrigatório
+      nomeProduto: ['', Validators.required], // Campo obrigatório
+      quantidade: [null, [Validators.required, Validators.min(1)]], // Campo obrigatório com valor mínimo de 1
+      observacao: [''], // Campo opcional
+    });
+  }
+
+  // Método chamado no envio do formulário
+  onSubmit() {
+    // Verifica se o formulário não é nulo e é válido
+    if (this.quotationForm && this.quotationForm.valid) {
+      // Acessa os valores do formulário com non-null assertion (!)
+      console.log(this.quotationForm.value!);
+    }
+  }
+}

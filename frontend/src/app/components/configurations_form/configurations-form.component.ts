5d5f1ab2148fa6c560e4038228690acd1c4b209f
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-08 00:15:54 -0300

    Refactor form components: Update form layouts and styles

--- frontend/src/app/components/configurations_form/configurations-form.component.ts
+++ frontend/src/app/components/configurations_form/configurations-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-configurations-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './configurations-form.component.html',
   styleUrl: './configurations-form.component.css'
 })

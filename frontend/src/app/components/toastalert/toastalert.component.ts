add946f3220e1fab41924f9a46953020366fb6c7
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-08 23:31:23 -0300

    Update Angular version to v18.2.3

--- frontend/src/app/components/toastalert/toastalert.component.ts
+++ frontend/src/app/components/toastalert/toastalert.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import {MatCardModule} from "@angular/material/card"
-import { MatButtonModule } from "@angular/material/button"
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-toastalert',
   standalone: true,
   imports: [MatCardModule, MatButtonModule],
   templateUrl: './toastalert.component.html',
-  styleUrl: './toastalert.component.css',
+  styleUrls: ['./toastalert.component.css'],
 })
 export class ToastalertComponent {
-  constructor(private toastr: ToastrService) {
-
-  }
+  constructor(private toastr: ToastrService) {}
 
   showSucess() {
-    this.toastr.success("Enviada com sucesso!", "Sucesso!", {})
+    this.toastr.success('Enviada com sucesso!', 'Sucesso!', {
+      positionClass: 'toast-top-right', // Ajuste a posição conforme necessário
+    });
   }
 }

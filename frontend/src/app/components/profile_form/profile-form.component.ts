fbada11cf1e4a934806815a877d61b28cc82b061
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-09-08 22:25:30 -0300

    Refactor profile form component: Add notification alert and close button logic

--- frontend/src/app/components/profile_form/profile-form.component.ts
+++ frontend/src/app/components/profile_form/profile-form.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @Component({
   selector: 'app-profile-form',
   standalone: true,
-  imports: [NgxMaskDirective, NgxMaskPipe],
+  imports: [NgxMaskDirective, NgxMaskPipe, CommonModule],
   templateUrl: './profile-form.component.html',
   styleUrl: './profile-form.component.css',
 })
-export class ProfileFormComponent {}
+export class ProfileFormComponent {
+  showNotificationAlert: boolean;
+
+  constructor() {
+    const notificationsProfilePage = localStorage.getItem(
+      'notificationsProfilePage'
+    );
+    this.showNotificationAlert = !notificationsProfilePage;
+  }
+
+  // Função de fechar o alerta
+  closeButton() {
+    this.showNotificationAlert = false;
+    localStorage.setItem('notificationsProfilePage', 'true');
+  }
+}
